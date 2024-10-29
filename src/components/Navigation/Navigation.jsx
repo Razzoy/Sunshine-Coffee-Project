4a2eadfb070d0c6180eba64d41b7966123283693
Razzoy <1081892@ucn.dk>
2024-10-29 08:55:40 +0100

    added png

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,18 @@
+import { NavLink } from "react-router-dom";
+import style from './Navigation.module.scss'
 
 export function Navigation() {
   return (
-    <div>Navigation</div>
+    <>
+      <nav className={style.navbarStyle}>
+        <ul>
+          <li>
+            <NavLink style={({ isActive }) => isActive ? { textDecoration: 'underline' } : { textDecoration: 'none' }} to={'/'}>HOME</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={style.coffeeIcon}>
+      </div>
+    </>
   )
 }
